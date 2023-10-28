2828a1696985fde0c0650d354f4eb0414897563a
Rico <r@rico.wtf>
2023-10-28 20:05:00 +0200

    standalone algorythm

--- app.js
+++ app.js
@@ -1,19 +1,18 @@
 const puppeteer = require('puppeteer');
 const express = require('express');
 const bodyParser = require('body-parser');
+// const fs = require('fs');
 
 const app = express();
 app.use(express.static('public'));
+
 
 // Use body-parser middleware to parse JSON requests
 app.use(bodyParser.json());
 
 // /ping endpoint
-app.get('/ping.html', (req, res) => {
-    res.sendFile(__dirname + '/public/ping.html');
-});
 app.get('/ping', (req, res) => {
-    res.status(200).send('pong!');
+    res.sendFile(__dirname + '/public/ping.html');
 });
 
 // Endpoint to generate DBML
@@ -21,37 +20,79 @@ app.get('/generate-dbml', async (req, res) => {
     console.log('Received request to generate DBML.');
     // const { url } = req.body;
     const { url } = req.query
+    res.set('Content-Type', 'application/json');
 
     if (!url) {
         console.log('URL missing in query params.');
         return res.status(400).send('URL is required.');
     }
 
+    console.log('Launching puppeteer browser.');
+    const browser = await puppeteer.launch({
+        args: [
+            "--disable-setuid-sandbox",
+            "--no-sandbox",
+            "--single-process",
+            "--no-zygote",
+        ],
+        headless: "new"
+    });
+    
+   
+
     console.log(`Extracting Bubble Data from URL: ${url}`);
     try {
-        const rawData = await extractBubbleData(url);
+        const rawData = await extractBubbleData(url, browser, res);
         console.log('Raw Data extracted. Generating DBML syntax');
+        // console.log("rawData", rawData);
+        // res.set('Content-Type', 'Application/json');
+        // res.send(rawData);
+        // const dbmlData = generateDbDiagramSyntax(rawData);
         console.log('Sending DBML data response.');
-        res.set('Content-Type', 'text/plain');
-        res.send(rawData);
+        return res.status(200).json({
+            error: rawData.error,
+            url: url,
+            diagram: rawData.diagram,
+            isBubbleApp: rawData.isBubbleApp,
+          });
     } catch (error) {
         console.error("Error encountered:", error);
-        res.status(500).send(`Error encountered: ${error.message}`);
+        res.status(500).json({
+            error: error.message,
+            url: url,
+            diagram: "",
+            isBubbleApp: "",
+          });
+    } finally {
+        console.log("closing puppeteer browser")
+        await browser.close();
     }
 });
 
-async function extractBubbleData(url) {
-    console.log('Launching puppeteer browser.');
-    const browser = await puppeteer.launch({ args: ['--no-sandbox'], headless: "new" });
+async function extractBubbleData(url, browser, res) {
+    const result = {}
     const page = await browser.newPage();
 
     page.setDefaultNavigationTimeout(60000); // 60 seconds
 
     console.log(`Navigating to page: ${url}`);
     await page.goto(url);
+    
+    // check if the url is a Bubble app by checking if the appquery object exists
+    const isBubbleApp = await page.evaluate(() => {
+      return typeof appquery === "function";
+    });
+    console.log("isBubbleApp", isBubbleApp);
+    result.isBubbleApp = isBubbleApp;
+    
+    if (isBubbleApp === false) {
+      console.log(`does not seem to be a Bubble app ${url}`);
+      result.error = "not a Bubble app"
+      return result;
+    }
 
     console.log('Evaluating the page.');
-    const result = await page.evaluate(() => {
+    const diagram = await page.evaluate(() => {
         const types = appquery.custom_types();
         const option_sets = appquery.option_sets();
         const dbdiagram_json = {};
@@ -72,7 +113,7 @@ async function extractBubbleData(url) {
             } else {
                 api += `Table ${table} {\n\t${key} int\n}\n\n`;
             }
-            
+
         }
 
         dbdiagram_dbml += ``;
@@ -83,7 +124,7 @@ async function extractBubbleData(url) {
 
             // add the Table and the default Bubble fields
             dbdiagram_dbml +=
-                `Table public.${table_name} {\n\t_id text [pk, unique]\n\t"Modified date" date\n\t"Created data" date`;
+                `Table custom.${table_name} {\n\t_id text [pk, unique]\n\t"Modified date" date\n\t"Created data" date`;
 
             // if it's the user table, add the email field
             if (table_name == "user") {
@@ -197,11 +238,99 @@ async function extractBubbleData(url) {
         return dbdiagram_dbml;
     });
 
-    console.log('Closing the puppeteer browser.');
-    await browser.close();
-
+    result.diagram = diagram;
     return result;
 }
+
+function generateDbDiagramSyntax(raw_result) {
+    const data = JSON.parse(raw_result);
+    // const data = raw_result;
+
+    if (!data || !Array.isArray(data) || data.length === 0) {
+        throw new Error("Invalid data format.");
+    }
+
+    if (!data[0].json || !data[0].json.cache) {
+        throw new Error("Invalid data structure.");
+    }
+
+    data.forEach((item) => {
+        const table_name = data.name();
+
+        // add the Table and the default Bubble fields
+        dbdiagram_dbml +=
+            `Table custom.${table_name} {\n\t_id text [pk, unique]\n\t"Modified date" date\n\t"Created data" date`;
+
+        // if it's the user table, add the email field
+        if (table_name == "user") {
+            dbdiagram_dbml += `\n\temail text [unique]`;
+        }
+
+        const child = item.json.cache["%f3"];
+        for (let long_key in child) {
+            // console.log("child", child[long_key]);
+            // console.log("long_key", long_key);
+            if (child.hasOwnProperty(long_key)) {
+                // Access the object using the property long_key
+                const innerObject = child[long_key];
+                let key = innerObject["%d"];
+                if (!key.includes(" - deleted")) {
+                    let value = innerObject["%v"];
+                    let new_value = value; // use this variable to manipulate and put the value back into the text file
+                    let connection_direction = "-";
+                    let log_string = "logging starting";
+                    if (value.includes("list.")) {
+                        // log_string += "\nvalue includes list";
+                        new_value = value.replace("list.", "");
+                        // log_string += "\nstarting key: " + key + "\nvalue: " + value +  "\nnew_value: " + new_value;
+                        if (value.includes("custom.")) {
+                            // log_string += "\nkey has 'custom.'";
+                            new_value = new_value.replace("custom.", "");
+                            // add the reference to the references variable
+                            connection_direction = ">";
+                            new_value += ` [ref: ${connection_direction} ${new_value}._id]`;
+                        } else if (value.includes("user")) {
+                            connection_direction = ">";
+                            new_value += ` [ref: ${connection_direction} user._id]`;
+                        } else {
+                            new_value += "[]";
+                        };
+                    }
+                    // log_string += "\nending:\nkey: " + key + "\nvalue: " + value + "\nnew_value: " + new_value;
+                    // console.log(log_string);
+                    //handle api key
+                    // TODO change it to make it point to the API schema -- meaning, change the normal one to include public behind every table name
+                    if (value.includes("api.")) {
+                        // console.log("value includes api, calling function to create table");
+                        value = value.replace("api.", "");
+
+                        const dotIndex = value.indexOf(".");
+                        const new_table = value.substring(0, dotIndex);
+                        const new_key = value.substring(dotIndex + 1);
+
+                        // console.log(new_table); // "stripe"
+                        // console.log(new_key);
+
+                        api_search(new_table, new_key.replace(/\[\]/g, ""), table_name, key);
+                    }
+
+                    value = value.replace("option.", "option_");
+                    // console.log(`\n\t"${key}" ${new_value}`);
+                    // console.log(`${new_value}`);
+                    dbdiagram_dbml += `\n\t"${key}" ${new_value}`;
+                }
+                // dbdiagram_dbml += `\n`;
+
+                // Access the properties of the inner object
+                //console.log("innerObject", innerObject)
+            }
+        }
+        dbdiagram_dbml += `\n}\n\n`;
+    });
+
+    return output;
+}
+
 
 function quoteIfNeeded(type) {
     if (type.includes('.')) {
---
+++ createDiagramJson.js
@@ -0,0 +1,55 @@
+const data = appquery.custom_types() || [];
+const types = {}
+
+data.forEach((item) => {
+	var name = item.json.__name;
+	var fieldsObj = item.json.cache["%f3"];
+	var fields = []
+	for (let field in fieldsObj) {
+		fields.push(field = {
+			name: item.json.cache["%f3"][field]["%d"],
+			type: item.json.cache["%f3"][field]["%v"]
+	})}
+	types[name] = {
+		name: name,
+		path: item.json.__path,
+		fields: fields
+	}
+})
+
+// console.log(types)
+
+const osData = appquery.option_sets();
+const os = {};
+
+osData.forEach((item) => {
+	var name = item.json.__name;
+	var osObj = item.json.cache;
+	var osAttributes = item.json.cache.attributes;
+	var osValues = item.json.cache.values;
+	var attributes = []
+	let values = [];
+	for (let attribute in osAttributes) {
+		// console.log("attribute", osAttributes[attribute])
+		attributes.push(attribute = {
+			name: osAttributes[attribute]["%d"],
+			value: osAttributes[attribute]["%v"],
+		})
+	}
+	for (let value in osValues) {
+		values.push(osValues[value])
+	}
+	os[name] = {
+		name: name,
+		path: item.json.__path,
+		attributes: attributes,
+		values: values
+	}
+})
+
+// console.log(os);
+const db = {
+	custom_types: types,
+	option_sets: os
+}
+console.log(db);
