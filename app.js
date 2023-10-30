6f17ff0fcb89252bf03b03f82524ab2e21f16cfc
Rico Trevisan <r@rico.wtf>
2023-10-30 06:40:57 +0100

    update from desktop

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const puppeteer = require('puppeteer');
 const express = require('express');
 const bodyParser = require('body-parser');
+const createDiagramJson = require('./createDiagramJson');
 
 const app = express();
 app.use(express.static('public'));
@@ -43,10 +44,7 @@ app.get('/generate-dbml', async (req, res) => {
     try {
         const rawData = await extractBubbleData(url, browser, res);
         console.log('Raw Data extracted. Generating DBML syntax');
-        // console.log("rawData", rawData);
-        // res.set('Content-Type', 'Application/json');
-        // res.send(rawData);
-        // const dbmlData = generateDbDiagramSyntax(rawData);
+
         console.log('Sending DBML data response.');
         return res.status(200).json({
             error: rawData.error,
@@ -91,215 +89,11 @@ async function extractBubbleData(url, browser, res) {
     }
 
     console.log('Evaluating the page.');
-    
-    const db = await page.evaluate(() => {
-        const data = appquery.custom_types() || [];
-        const types = []
-        
-        data.forEach((item) => {
-            var name = item.json.__name;
-            var fieldsObj = item.json.cache["%f3"];
-            var fields = []
-            for (let field in fieldsObj) {
-                fields.push(field = {
-                    name: item.json.cache["%f3"][field]["%d"],
-                    type: item.json.cache["%f3"][field]["%v"]
-                })
-            }
-            types.push(name = {
-                name: name,
-                path: item.json.__path,
-                fields: fields
-            })
-        })
-        
-        const osData = appquery.option_sets();
-        const os = [];
-        
-        osData.forEach((item) => {
-            var name = item.json.__name;
-            var osObj = item.json.cache;
-            var osAttributes = item.json.cache.attributes;
-            var osValues = item.json.cache.values;
-            var attributes = []
-            let values = [];
-            for (let attribute in osAttributes) {
-                // console.log("attribute", osAttributes[attribute])
-                attributes.push(attribute = {
-                    name: osAttributes[attribute]["%d"],
-                    value: osAttributes[attribute]["%v"],
-                })
-            }
-            for (let value in osValues) {
-                values.push(osValues[value])
-            }
-            os.push(name = {
-                name: name,
-                path: item.json.__path,
-                attributes: attributes,
-                values: values
-            })
-        })
-        
-        // console.log(os);
-        const db = {
-            custom_types: types,
-            option_sets: os
-        }
-        console.log(db);
-        return db
-    })
+    const db = await createDiagramJson(page);
+
     console.log("db", db);
     
-    const diagram = await page.evaluate(() => {
-        const types = appquery.custom_types();
-        const option_sets = appquery.option_sets();
-        const dbdiagram_json = {};
-        let dbdiagram_dbml = "";
-        let option_sets_text = "\/\/ option_sets\n";
-        let api = "\/\/ api calls datatype\n";
 
-        function api_search(table, key, current_table, current_key) {
-            // TODO need to add to the references file.
-            // maybe need to pass the `current_table`
-            //
-            console.log(`running api_search(${table}, ${key})`);
-            if (api.includes(`Table ${table} {`)) {
-                const insertIndex = api.indexOf(`Table ${table} {`) +
-                    `Table ${table} {`.length;
-                api = api.slice(0, insertIndex) + `\n\t${key} int` +
-                    api.slice(insertIndex);
-            } else {
-                api += `Table ${table} {\n\t${key} int\n}\n\n`;
-            }
-
-        }
-
-        dbdiagram_dbml += ``;
-        // iterate over the types
-        types.forEach((item) => {
-            dbdiagram_json[item.name()] = item.json.cache["%f3"];
-            const table_name = item.name();
-
-            // add the Table and the default Bubble fields
-            dbdiagram_dbml +=
-                `Table custom.${table_name} {\n\t_id text [pk, unique]\n\t"Modified date" date\n\t"Created data" date`;
-
-            // if it's the user table, add the email field
-            if (table_name == "user") {
-                dbdiagram_dbml += `\n\temail text [unique]`;
-            }
-
-            const child = item.json.cache["%f3"];
-            for (let long_key in child) {
-                // console.log("child", child[long_key]);
-                // console.log("long_key", long_key);
-                if (child.hasOwnProperty(long_key)) {
-                    // Access the object using the property long_key
-                    const innerObject = child[long_key];
-                    let key = innerObject["%d"];
-                    // skip if it's marked as deleted
-                    if (!key.includes(" - deleted")) {
-                        let value = innerObject["%v"];
-                        let new_value = value; // use this variable to manipulate and put the value back into the text file
-                        let connection_direction = "-";
-                        let log_string = "logging starting";
-                        if (value.includes("list.")) {
-                            // log_string += "\nvalue includes list";
-                            new_value = value.replace("list.", "");
-                            // log_string += "\nstarting key: " + key + "\nvalue: " + value +  "\nnew_value: " + new_value;
-                            if (value.includes("custom.")) {
-                                // log_string += "\nkey has 'custom.'";
-                                // new_value = new_value.replace("custom.", "");
-                                // add the reference to the references variable
-                                connection_direction = ">";
-                                new_value +=
-                                    ` [ref: ${connection_direction} ${new_value}._id]`;
-                            } else if (value.includes("user")) {
-                                connection_direction = ">";
-                                new_value += ` [ref: ${connection_direction} user._id]`;
-                            } else {
-                                new_value += "[]";
-                            }
-                        }
-                        // log_string += "\nending:\nkey: " + key + "\nvalue: " + value + "\nnew_value: " + new_value;
-                        // console.log(log_string);
-                        //handle api key
-                        // TODO change it to make it point to the API schema -- meaning, change the normal one to include public behind every table name
-                        if (value.includes("api.")) {
-                            // console.log("value includes api, calling function to create table");
-                            value = value.replace("api.", "");
-
-                            const dotIndex = value.indexOf(".");
-                            const new_table = value.substring(0, dotIndex);
-                            const new_key = value.substring(dotIndex + 1);
-
-                            // console.log(new_table); // "stripe"
-                            // console.log(new_key);
-
-                            api_search(
-                                new_table,
-                                new_key.replace(/\[\]/g, ""),
-                                table_name,
-                                key,
-                            );
-                        }
-
-                        value = value.replace("option.", "option_");
-                        // console.log(`\n\t"${key}" ${new_value}`);
-                        // console.log(`${new_value}`);
-                        dbdiagram_dbml += `\n\t"${key}" ${new_value}`;
-                    }
-                    // dbdiagram_dbml += `\n`;
-
-                    // Access the properties of the inner object
-                    //console.log("innerObject", innerObject)
-                }
-            }
-            dbdiagram_dbml += `\n}\n\n`;
-        });
-
-        // iterate over the option sets
-        option_sets.forEach((item) => {
-            const table_name = item.name();
-            dbdiagram_json[table_name] = item.json.cache["%f3"];
-            option_sets_text +=
-                `Table option.${table_name} {\n\t"Display" text [pk, unique]`;
-            const child = item.json.cache["attributes"];
-            for (let long_key in child) {
-                if (child.hasOwnProperty(long_key)) {
-                    const innerObject = child[long_key];
-                    let key = innerObject["%d"];
-                    if (!key.includes("- deleted")) {
-                        // Access the object using the property long_key
-                        let value = innerObject["%v"];
-                        if (value.includes("list.")) {
-                            value = value.replace(/^list\./, "");
-                            value += "[]";
-                        }
-                        // value = value.replace("option.", "option_");
-                        option_sets_text += `\n\t"${key}" ${value}`;
-
-                        // Access the properties of the inner object
-                        //console.log("innerObject", innerObject)
-                    }
-                }
-            }
-            option_sets_text += `\n}\n\n`;
-        });
-
-        // add option sets
-        dbdiagram_dbml += `\n${option_sets_text}`;
-
-        // add the api to the bottom
-        dbdiagram_dbml += `\n${api}`;
-
-        return dbdiagram_dbml;
-    });
-
-    result.diagram = diagram;
-    return result;
-}
 
 function generateDbDiagramSyntax(raw_result) {
     const data = JSON.parse(raw_result);
